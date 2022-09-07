1430eff400b474e40c0f4acefd483f1ded920546
Hayko S <blabla@gmail.com>
2022-09-07 18:33:55 +0400

    vse

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# locals {
+#   cluster_name = "Im_cluster"
+# }
---
+++ secrets.tf
@@ -0,0 +1,8 @@
+variable "rds_username" {
+  description = "rds db username"
+  default     = "djangoproject"
+}
+variable "rds_password" {
+  description = "rds db password"
+  default     = "secret01" # uzum e 8 nishani gaxtnabar
+}
